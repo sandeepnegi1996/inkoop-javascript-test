6d7f36e244cef80969855a9987ccef706328803a
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-11-06 19:26:26 +0530

    able to generate random values

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log("hello world");
+
+const generateSelector = document.querySelector("#gen");
+const outputDiv = document.querySelector("#output");
+const inputRandom = document.querySelector("#gen-num");
+const refershing = document.querySelector("#refreshing");
+
+function randomText() {
+  console.log("button clicked");
+  const randomNum = Math.round(Math.random() * 1000000);
+  console.log(randomNum);
+  inputRandom.innerHTML = randomNum;
+}
+
+function refershingText() {
+  console.log("checking");
+
+  for (let index = 0; index <= 5; index++) {
+    setInterval(function() {
+      refershing.innerHTML = "Refreshing in " + index + " Seconds";
+    }, 1000);
+  }
+}
+function main() {
+  generateSelector.addEventListener("click", function() {
+    randomText();
+    refershingText();
+  });
+}
+
+main();
